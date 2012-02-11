8c13743012634696e31b89ea68912298c5c55984
Kang Chen <kang@posterous-inc.com>
2012-02-10 21:08:14 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dimsum::Application.config.secret_token = '6869f7cb789f95a8e0167226a49441d324a95c9cb1a0b4b0dbd61812057e39c46f0407448a80683134c97f7f7862cb75d555cd5484108f98f0e094be3ac29a69'
