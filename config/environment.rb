2b5b6fd3aa0e5800abd274e3c1d3d014b8feab78
Anjali Shenoy <anjshenoy@gmail.com>
2012-02-16 14:09:26 -0800

    Removed google maps api key from config and added it to the require lib class

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 FOURSQUARE_OAUTH_TOKEN = "NGRXLIRVOWOHCQENLOKOELLSTC5YX3QJIPJ3QFSQXSQ51SF2"
 #GOOGLE_API_KEY = "ABQIAAAApVHLA8Qx_XkMWZZEh0-o-hRHs9R0b-OwVoDaAEGKH4zKKqwRkhRzAXIHZeZwUex8Vuyv_b-jZwPe_g"
-GOOGLE_MAPS_API_KEY = "AIzaSyBbwI24flYLtaYgjzPhydunHVhTbgg15Zo"
+
 
 # Initialize the rails application
 Dimsum::Application.initialize!
