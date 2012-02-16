e3445a38f6a025f9da771d344cbb8881d70b4433
Anjali Shenoy <anjshenoy@gmail.com>
2012-02-16 14:10:49 -0800

    list now generates audio files during search

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,21 +1,30 @@
-require 'assets/ext/google_maps'
 class List < ActiveRecord::Base
   has_many :items
 
+  before_create :pass_through_nlp
+
+  VOICES = ["Agnes","Albert","Alex","Bad News","Bahh","Bells","Boing","Bruce","Bubbles","Cellos","Deranged","Fred","Good News","Hysterical","Junior","Kathy","Pipe Organ","Princess","Ralph","Trinoids","Vicki","Victoria","Whisper","Zarvox"]
+
   def self.by_lat_long(lat, long)
     matches = []
-    venues.each do |venue_name|
-      place = Ext::GoogleMaps.venue("#{lat},#{long}", venue_name)
+    List.all.each do |list|
+      place = Ext::GoogleMaps.venue("#{lat},#{long}", list.location, list.category)
       unless place.nil?
-        matches << [List.where(:location => venue_name).first, place]
+        matches << [list, place]
+        display_text = "You are near #{place.name}. You had to #{list.subject}"
+        IO.popen("say -v #{VOICES.sample} -o #{Rails.public_path}/audio/#{list.id}.mp4 #{display_text}")
       end
     end
     return matches
   end
 
   private
-  def self.venues
-    List.all.collect(&:location)
+
+  def pass_through_nlp
+    t = NLProcessor::Task.new(self.original_text)
+    self.subject = t.subject
+    self.location = t.location
+    self.category = t.category
   end
 
 end
